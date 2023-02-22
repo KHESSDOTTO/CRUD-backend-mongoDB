576a4e1f619555305c286fa6bb8b3eda0fa1b3c7
Khess S Dotto <khessdotto@gmail.com>
2023-02-22 18:29:48 -0300

    Created grain schema

--- models/grains.models.js
+++ models/grains.models.js
@@ -1,1 +1,30 @@
 import { Schema, model } from "mongoose";
+
+const grainSchema = new Schema(
+  {
+    name: {
+      type: string,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: string,
+      required: true,
+      trim: true,
+    },
+    kcalper100g: {
+      type: number,
+      required: true,
+    },
+    istasty: {
+      type: boolean,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const grainsModel = model("Grain", grainSchema);
+export default grainsModel;
