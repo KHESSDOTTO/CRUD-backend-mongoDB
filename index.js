796ad1cee392f68485e7b9cfa0d64dcb639274df
Khess S Dotto <khessdotto@gmail.com>
2023-02-22 18:45:59 -0300

    Creating Routes.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { connectToDb } from "./config/db.config.js";
+import grainsRouter from "./routes/grains.routes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use("/grains", grainsRouter);
 
 connectToDb();
 
---
+++ routes/grains.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import grainsModel from "../models/grains.models.js";
+
+const grainsRouter = express.Router();
+
+// Fazer as rotas - CRUD
+
+export default grainsRouter;
